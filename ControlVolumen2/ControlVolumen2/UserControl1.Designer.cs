520e103d308153ef960e7246b0332e01aa96a106
Leandro <44428549+lerubo@users.noreply.github.com>
2018-10-24 15:37:10 +0100

    24/10/2018

---
+++ ControlVolumen2/ControlVolumen2/UserControl1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ControlVolumen2
+{
+    partial class Controlador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img_Altavoz = new System.Windows.Forms.PictureBox();
+            this.lbl_Porcentaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Altavoz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img_Altavoz
+            // 
+            this.img_Altavoz.Image = global::ControlVolumen2.Properties.Resources.altavozMedio;
+            this.img_Altavoz.Location = new System.Drawing.Point(0, 0);
+            this.img_Altavoz.Name = "img_Altavoz";
+            this.img_Altavoz.Size = new System.Drawing.Size(100, 100);
+            this.img_Altavoz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_Altavoz.TabIndex = 0;
+            this.img_Altavoz.TabStop = false;
+            this.img_Altavoz.Click += new System.EventHandler(this.img_Altavoz_Click);
+            // 
+            // lbl_Porcentaje
+            // 
+            this.lbl_Porcentaje.AutoSize = true;
+            this.lbl_Porcentaje.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Porcentaje.Font = new System.Drawing.Font("Decker", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Porcentaje.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_Porcentaje.Location = new System.Drawing.Point(106, 39);
+            this.lbl_Porcentaje.Name = "lbl_Porcentaje";
+            this.lbl_Porcentaje.Size = new System.Drawing.Size(49, 19);
+            this.lbl_Porcentaje.TabIndex = 1;
+            this.lbl_Porcentaje.Text = "0000";
+            // 
+            // Controlador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbl_Porcentaje);
+            this.Controls.Add(this.img_Altavoz);
+            this.MaximumSize = new System.Drawing.Size(500, 100);
+            this.MinimumSize = new System.Drawing.Size(500, 100);
+            this.Name = "Controlador";
+            this.Size = new System.Drawing.Size(500, 100);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Controlador_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Controlador_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Controlador_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.img_Altavoz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img_Altavoz;
+        private System.Windows.Forms.Label lbl_Porcentaje;
+    }
+}
---
+++ ControlVolumen2/ControlVolumen2/UserControl1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace ControlVolumen2
+{
+    [ToolboxBitmap(typeof(Controlador), "audio-speaker-on.png")]
+    public partial class Controlador : UserControl
+    {
+        int porcentaje = 50;
+        Boolean sonido = true;
+        Brush colorVolumen = new SolidBrush(Color.LightGreen);
+        Boolean arrastre = false;
+        int alto;
+        int ancho;
+        int puntoVolumen;
+        Point[] parametros = new Point[3];
+
+        public Controlador()
+        {
+            InitializeComponent();
+            alto = this.Height;
+            ancho = this.Width;
+            parametros[0] = new System.Drawing.Point(img_Altavoz.Width, alto);
+            nuevoTriangulo(porcentaje);
+            lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.FillPolygon(colorVolumen, parametros, System.Drawing.Drawing2D.FillMode.Alternate);
+
+        }
+
+        private void Controlador_MouseDown(object sender, MouseEventArgs e)
+        {
+            arrastre = true;
+            if ((arrastre) && (e.X < this.Width) && (e.X > 0) && (e.Y < this.Height) && (e.Y > 0))
+            {
+                nuevoTriangulo(e);
+            }
+        }
+
+        private void Controlador_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((arrastre) && (e.X < this.Width) && (e.X > 0) && (e.Y < this.Height) && (e.Y > 0))
+            {
+                nuevoTriangulo(e);
+            }
+        }
+
+        private void Controlador_MouseUp(object sender, MouseEventArgs e)
+        {
+            arrastre = false;
+        }
+
+        private void img_Altavoz_Click(object sender, EventArgs e)
+        {
+            if (sonido)
+            {
+                img_Altavoz.Image = Properties.Resources.altavozSilenciado;
+                sonido = false;
+                nuevoTriangulo(0);
+                lbl_Porcentaje.Text = ("0" + "%");
+            }
+            else
+            {
+                nuevoTriangulo(porcentaje);
+                lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+                sonido = true;
+            }
+        }
+
+        private void nuevoTriangulo(MouseEventArgs e)
+        {
+            int porcentajeW = (((e.X - img_Altavoz.Width) * 100) / (ancho - img_Altavoz.Width));
+            int porcentajeH = ((porcentajeW * alto) / 100);
+            puntoVolumen = (alto - porcentajeH);
+
+            parametros[1] = new Point(e.X, alto);
+            parametros[2] = new Point(e.X, puntoVolumen);
+
+            porcentaje = porcentajeW;
+
+          
+
+            if (porcentajeW == 0)
+            {
+                img_Altavoz.Image = Properties.Resources.altavozSilenciado;               
+                lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+                sonido = false;
+            }
+
+            if ((porcentajeW > 0) && (porcentajeW <= 50))
+            {
+                img_Altavoz.Image = Properties.Resources.altavozBajo;
+                colorVolumen = new SolidBrush(Color.LightGreen);             
+                lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+                sonido = true;
+            }
+
+            if ((porcentajeW > 50) && (porcentajeW <= 90))
+            {
+                img_Altavoz.Image = Properties.Resources.altavozMedio;
+                colorVolumen = new SolidBrush(Color.FromArgb(255, (255), (205), (0)));
+                porcentaje = porcentajeW;
+                lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+                sonido = true;
+            }
+
+            if ((porcentajeW > 90) && (porcentajeW <= 100))
+            {
+                img_Altavoz.Image = Properties.Resources.altavozAlto;
+                colorVolumen = new SolidBrush(Color.IndianRed);
+                if (porcentajeW > 96) { porcentaje += 1; }
+                lbl_Porcentaje.Text = (porcentaje.ToString() + "%");
+                sonido = true;
+            }
+            Invalidate();
+
+            var evento = this.PorcentajeCambiado;
+            if (evento != null)
+            {
+                evento(this, new PorcentajeArgs(porcentaje));
+                MessageBox.Show("cambio de porcenjate ");
+            }
+        }
+        private void nuevoTriangulo(int posicion)
+        {
+            int p = posicion;
+            int porcentajeH = ((p * alto) / 100);
+            puntoVolumen = (alto - porcentajeH);
+            int x = ((p * (ancho - img_Altavoz.Width)) / 100);
+            x = x + img_Altavoz.Width;
+
+            parametros[1] = new Point(x, alto);
+            parametros[2] = new Point(x, puntoVolumen);
+
+            if ((posicion > 0) && (posicion <= 50)) {
+                img_Altavoz.Image = Properties.Resources.altavozBajo;
+                colorVolumen = new SolidBrush(Color.LightGreen);
+            }
+            if ((posicion > 50) && (posicion <= 90))
+            {
+                img_Altavoz.Image = Properties.Resources.altavozMedio;
+                colorVolumen = new SolidBrush(Color.FromArgb(255, (255), (205), (0)));
+            }
+            if ((posicion > 90) && (posicion <= 100))
+            {
+                img_Altavoz.Image = Properties.Resources.altavozAlto;
+                colorVolumen = new SolidBrush(Color.IndianRed);
+            }
+            Invalidate();
+        }
+
+        public class PorcentajeArgs : EventArgs
+        {
+            private int porcentajes = 50;
+            public PorcentajeArgs(int nuevoPorcentaje)
+            {
+                if (this.porcentajes != nuevoPorcentaje){
+                    MessageBox.Show("Aqui");
+                }
+                porcentajes = nuevoPorcentaje;                   
+            }
+            public int nuevoPorcentaje
+            {
+                get
+                {
+                    return porcentajes;
+                }
+            }
+        }
+        public delegate void cambioPorcentaje(Object sender, PorcentajeArgs e);
+        public event cambioPorcentaje PorcentajeCambiado;
+    }
+}
